5bdecbc276486dfd6c422fbe44faaa335ada230b
pietoto <tanya.vostrikova.2006@mail.ru>
2023-04-30 17:06:08 +0400

    ---
    ---

---
+++ model_form.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_login import LoginManager
+
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main():
+    db_session.global_init("db/mars_explorer_1.db")
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        session = db_session.create_session()
+        return session.query(User).get(user_id)
+
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
